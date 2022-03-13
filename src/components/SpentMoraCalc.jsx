15c1b908620feea276594ecccc49c409dbb34d53
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2022-03-13 15:40:20 +0100

    Removed unused import from SpentMoraCalc

--- src/components/SpentMoraCalc.jsx
+++ src/components/SpentMoraCalc.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import UploadFile from './UploadFile';
 import MoraResults from './MoraResults';
 import Weapon from './Weapon';
